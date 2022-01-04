65bc346c8c4afdfcd2d1e1db2a4dcd3b1c26cbf0
HassanMak29 <76273304+HassanMak29@users.noreply.github.com>
2022-01-04 15:27:17 +0100

    Implemented stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,7 @@ import { login, logout } from './login';
 import { displayMap } from './mapbox';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 //DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -82,3 +83,6 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage.at, 20);
