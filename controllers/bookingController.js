32e761a71423714edcdfa26bac646c48536334ca
HassanMak29 <76273304+HassanMak29@users.noreply.github.com>
2022-10-21 23:16:22 +0100

    fixed success url of checkout session creation function

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -14,12 +14,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/?tour=${
-      req.params.tourId
-    }&user=${req.user.id}&price=${tour.price}&startDate=${new Date(
-      tour.startDates[0].date
-    ).getTime()}`,
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+    // success_url: `${req.protocol}://${req.get('host')}/?tour=${
+    //   req.params.tourId
+    // }&user=${req.user.id}&price=${tour.price}&startDate=${new Date(
+    //   tour.startDates[0].date
+    // ).getTime()}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
